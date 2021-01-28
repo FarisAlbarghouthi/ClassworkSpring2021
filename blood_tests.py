0b9ca0c98f8b9c4d5d8316f347d0ddefa26c86c7
FarisAlbarghouthi <faris.albarghouthi@gmail.com>
2021-01-28 15:55:16 -0500

    created simple text interface

---
+++ blood_tests.py
@@ -0,0 +1,13 @@
+def interface(): 
+
+	print("Blood test analysis")
+	while True:
+                print("\nOptions")
+                print("9-Quit")
+                choice = input("Enter an option: ")
+                if choice == "9":
+                        return
+
+interface()
+
+
