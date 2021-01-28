a095c6d010b658415a4d440b7f4822dd6ff6591e
FarisAlbarghouthi <faris.albarghouthi@gmail.com>
2021-01-28 16:36:01 -0500

    added LDL drivers

--- blood_tests.py
+++ blood_tests.py
@@ -9,6 +9,8 @@ def interface():
                         return
                 elif choice == "HDL":
                         HDL_driver()
+                elif choice == "LDL":
+                        LDL_driver()
                 return
 
 def HDL_driver():
@@ -25,4 +27,17 @@ def HDL_driver():
                 print("Your HDL is low")
         return
 
+def LDL_driver
+        LDL = int(input("Enter your LDL level: "))
+        if LDL>= 190:
+                print("Your LDL is very high")
+        elif 160<=LDL<=189:
+                print("Your LDL is high")
+        elif 130<=LDL<=159:
+                print("Your LDL is borderline high")
+        elif LDL<130:
+                print("Your LDL is normal")
+        return
+
+                
 interface()
