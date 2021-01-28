6a741c2cd8300173496bbd3f83bba0ea15359abd
FarisAlbarghouthi <faris.albarghouthi@gmail.com>
2021-01-28 16:27:29 -0500

    modified HDL addition

--- blood_tests.py
+++ blood_tests.py
@@ -7,7 +7,22 @@ def interface():
                 choice = input("Enter an option: ")
                 if choice == "9":
                         return
+                elif choice == "HDL":
+                        HDL_driver()
+                return
+
+def HDL_driver():
+        #get data
+        #analyze data
+        #output a result
+
+        HDL = int(input("Enter your HDL level: "))
+        if HDL>=60:
+                print("Your HDL is normal")
+        elif HDL>=40 & HDL <60:
+                print("Your HDL is borderline low")
+        elif HDL<40:
+                print("Your HDL is low")
+        return
 
 interface()
-
-
